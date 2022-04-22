6bacd9c3ab6818d6f54aa5c8ca832ae434873522
Carlos <80263269+cgallol@users.noreply.github.com>
2022-04-21 22:18:29 -0500

    dependencias rpi
    dependencias para raspberry pi

---
+++ dependencias.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt upgrade -y 
+sudo apt-get dist-upgrade -y
+sudo reboot
+sudo apt-get install python3-distutils
+sudo apt-get install python3-dev
+
+
+# pip3 
+https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+python -m pip install --upgrade pip
