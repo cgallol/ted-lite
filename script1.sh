f2cdc8826bd29c7cf465aca3fc16156e56be84f5
Carlos <80263269+cgallol@users.noreply.github.com>
2022-04-26 14:50:20 -0500

    comandos para ted1
    lista de comandos

---
+++ script1.sh
@@ -0,0 +1,16 @@
+sudo apt-get install unixodbc -y
+sudo apt-get install unixodbc-dev -y
+sudo apt-get install freetds-dev -y
+sudo apt-get install tdsodbc
+sudo apt-get install freetds-bin
+
+pip3 install RPi.GPIO
+pip3 install mysql-connector-python
+sudo pip3 install numpy
+
+sudo apt-get install python3-matplotlib -y
+sudo pip3 install pyodbc
+sudo apt-get install python3-pyodbc
+
+sudo apt-get install i2c-tools -y
+sudo apt-get install python3-smbus
