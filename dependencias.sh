665956902f3c728091fa4d79a57495063ad1f8d9
Carlos <80263269+cgallol@users.noreply.github.com>
2022-08-14 15:29:38 -0500

    Update dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -2,14 +2,17 @@ sudo apt-get update
 sudo apt upgrade -y 
 sudo apt-get dist-upgrade -y
 sudo reboot
-sudo apt-get install python3-distutils
-sudo apt-get install python3-dev
+sudo apt-get install python3-distutils -y
+sudo apt-get install python3-dev -y
 
 
 # PIP3 
-https://bootstrap.pypa.io/get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 # o usar este comando
-python -m pip install --upgrade pip
-# o usar este comando
 sudo apt-get install python3-pip -y
+# para actualizar
+python -m pip install --upgrade pip
+#agregar path
+export PATH="$HOME/.local/bin:$PATH"
+#ver path echo $PATH
