e0475b18143dec43dcd573601f0fcb961cf02206
Carlos <80263269+cgallol@users.noreply.github.com>
2022-08-14 19:11:40 -0500

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -4,13 +4,20 @@ sudo apt-get install freetds-dev -y
 sudo apt-get install tdsodbc
 sudo apt-get install freetds-bin
 
-pip3 install RPi.GPIO
-pip3 install mysql-connector-python
-sudo pip3 install numpy
-
-sudo apt-get install python3-matplotlib -y
-sudo pip3 install pyodbc
 sudo apt-get install python3-pyodbc
-
 sudo apt-get install i2c-tools -y
 sudo apt-get install python3-smbus
+
+sudo apt install python3-gpiozero
+
+
+sudo pip3 install mysql-connector-python
+
+
+# sudo pip3 install pyodbc  # no funciono
+sudo pip3 install numpy
+sudo apt-get install python3-matplotlib -y
+
+
+
+
