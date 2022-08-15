47cc0157fd5f2162416e5b45b0209697b7405729
Carlos <80263269+cgallol@users.noreply.github.com>
2022-08-15 14:50:12 -0500

    Update ted_lite.sh

--- ted_lite.sh
+++ ted_lite.sh
@@ -5,7 +5,7 @@ sudo apt-get install tdsodbc
 sudo apt-get install freetds-bin
 sudo apt-get install python3-pyodbc
 sudo apt-get install i2c-tools -y
-sudo apt install python3-gpiozero
+sudo apt-get install python3-gpiozero
 
 sudo pip3 install mysql-connector-python
 sudo pip3 install adafruit-ads1x15
@@ -15,9 +15,8 @@ sudo pip3 install python-decouple
 
 
 
-# sudo pip3 install pyodbc  # no funciono
-sudo pip3 install numpy
-sudo apt-get install python3-matplotlib -y
+#sudo pip3 install numpy
+#sudo apt-get install python3-matplotlib -y
 
 
 
