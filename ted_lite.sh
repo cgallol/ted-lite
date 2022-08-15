b7a38344e147ffb4317e88e3de62c808fde4b70b
Carlos <80263269+cgallol@users.noreply.github.com>
2022-08-14 20:36:07 -0500

    Update ted_lite.sh

--- ted_lite.sh
+++ ted_lite.sh
@@ -3,15 +3,12 @@ sudo apt-get install unixodbc-dev -y
 sudo apt-get install freetds-dev -y
 sudo apt-get install tdsodbc
 sudo apt-get install freetds-bin
-
 sudo apt-get install python3-pyodbc
 sudo apt-get install i2c-tools -y
-sudo apt-get install python3-smbus
-
 sudo apt install python3-gpiozero
 
-
 sudo pip3 install mysql-connector-python
+
 
 
 # sudo pip3 install pyodbc  # no funciono
