e90a2eadbc5f69a10c877ce57fd6d1591d4e1628
Carlos <80263269+cgallol@users.noreply.github.com>
2022-08-14 20:35:12 -0500

    Update requisitos.sh

--- requisitos.sh
+++ requisitos.sh
@@ -6,6 +6,11 @@ sudo apt-get install python3-distutils -y
 sudo apt-get install python3-dev -y
 sudo apt-get install git -y
 
+sudo apt-get install build-essential
+sudo apt-get install python3-dev
+sudo apt-get install python3-smbus
+
+
 
 # PIP3 
 wget https://bootstrap.pypa.io/get-pip.py
