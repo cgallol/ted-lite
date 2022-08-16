36f1724c1861543790d8f95588ad7fe54b577096
Carlos <80263269+cgallol@users.noreply.github.com>
2022-08-16 16:50:52 -0500

    Update ted_lite.sh

--- ted_lite.sh
+++ ted_lite.sh
@@ -7,10 +7,13 @@ sudo apt-get install python3-pyodbc
 sudo apt-get install i2c-tools -y
 sudo apt-get install python3-gpiozero
 sudo apt-get install mariadb-server -y
+sudo apt-get install libqmi-utils -y
+sudo apt-get install udhcpc
 
 sudo pip3 install adafruit-ads1x15
 sudo pip3 install python-decouple
 sudo pip3 install mysql-connector-python==8.0.29
+
 
 
 
@@ -22,3 +25,4 @@ sudo pip3 install mysql-connector-python==8.0.29
 
 
 
+ 
