74c049c3161059c4e6e8f7765676b1ccdfa181fe
Carlos <80263269+cgallol@users.noreply.github.com>
2022-08-17 11:31:30 -0500

    Update scri.sh

--- scri.sh
+++ scri.sh
@@ -16,9 +16,8 @@ sudo apt-get install python3-smbus
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo apt install python3-pip -y
-    # o usar este comando
-    sudo apt-get install python3-pip -y
+
 #agregar path
 export PATH="$HOME/.local/bin:$PATH"
-#ver path echo $PATH
+#ver path: echo $PATH
 
