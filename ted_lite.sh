bdf8b9b5a972c13225441551a922e43a84c4f19b
Carlos <80263269+cgallol@users.noreply.github.com>
2022-10-02 08:13:37 -0500

    Update ted_lite.sh

--- ted_lite.sh
+++ ted_lite.sh
@@ -10,12 +10,12 @@ sudo apt-get install mariadb-server -y
 sudo apt-get install libqmi-utils -y
 sudo apt-get install udhcpc
 sudo apt-get install python3-rpi.gpio o sudo pip3 install --ignore-installed rpi.gpio
+sudo apt-get install python3-scipy
 
 
 sudo pip3 install adafruit-ads1x15
 sudo pip3 install python-decouple
 sudo pip3 install mysql-connector-python==8.0.29
-sudo pip3 install scipy
 
 
 
